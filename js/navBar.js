4b1359360d775fac7ec9940591a0b68472ddb9d4
KimWithSomething <87281392+KimWithSomething@users.noreply.github.com>
2021-12-29 16:22:54 -0500

    Files added

---
+++ js/navBar.js
@@ -0,0 +1,18 @@
+var navBar = `
+<div class="menuBar">
+    <table>
+        <tr>
+            <td>
+                <a href="index.html"><button class="menuButton">Home page</button></a>
+            </td>
+            <td>
+                <a href="downloads.html"><button class="menuButton">Downloads</button></a>
+            </td>
+
+        </tr>
+    </table>
+</div>
+
+`
+
+document.write(navBar);
