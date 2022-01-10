4311307f047f4d06f35dc263b55a9b3d7d4345f1
KimWithSomething <87281392+KimWithSomething@users.noreply.github.com>
2022-01-10 14:49:33 -0500

    Add files via upload

--- js/navBar.js
+++ js/navBar.js
@@ -5,9 +5,13 @@ var navBar = `
             <td>
                 <a href="index.html"><button class="menuButton">Home page</button></a>
             </td>
+			<td>
+				<a href="release_logs.html"><button class="menuButton">Release Logs</button></a>
+			</td>
             <td>
                 <a href="downloads.html"><button class="menuButton">Downloads</button></a>
             </td>
+
 
         </tr>
     </table>
