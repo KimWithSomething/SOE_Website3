d65c9a64976bb4adbe67f594ebb936a00a7343e0
KimWithSomething <87281392+KimWithSomething@users.noreply.github.com>
2022-01-10 21:41:27 -0500

    Updated ALOT Things
    updated downloads 
    add new tabs
    started on mod fourms

--- js/navBar.js
+++ js/navBar.js
@@ -5,11 +5,14 @@ var navBar = `
             <td>
                 <a href="index.html"><button class="menuButton">Home page</button></a>
             </td>
-	    <td>
-		<a href="release_logs.html"><button class="menuButton">Release Logs</button></a>
-	    </td>
+			<td>
+				<a href="release_logs.html"><button class="menuButton">Release Logs</button></a>
+			</td>
             <td>
                 <a href="downloads.html"><button class="menuButton">Downloads</button></a>
+            </td>
+            <td>
+                <a href="Contributors.html"><button class="menuButton">Contributors</button></a>
             </td>
 
 
